7c0910d16cb4913df3ab70d9c80057f206e90fbc
Saikat Das <saikatdas0790@gmail.com>
2023-08-12 15:42:21 +0530

    chore: update binding URL

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ use axum::{response::Html, routing, Router};
 async fn main() {
     let app = Router::new().route("/", routing::get(handler));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Listening on {}", listener.local_addr().unwrap());
 
     axum::serve(listener, app).await.unwrap();
